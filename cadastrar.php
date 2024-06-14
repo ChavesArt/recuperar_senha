d5171c2f71dae513e9edff1aac1f90211cf9b684
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-06-14 14:34:17 -0300

    problemas com email duplicado consertado

--- cadastrar.php
+++ cadastrar.php
@@ -13,6 +13,11 @@ $sql = "INSERT INTO usuario (nome,email,senha) values ('$nome','$email','$senha'
 
 $resultado = mysqli_query($conexao,$sql);
 if($resultado == false){
+    if(mysqli_errno($conexao) == 1062){
+
+        echo"Email já cadastrado no sistema! Tente fazer o login ou faça a recuperação de senha.";
+        die();
+    }
 
     echo"Erro ao inserir novo usuário!"
     . mysqli_errno($conexao) .": "

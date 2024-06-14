9e6ae2ca3128b7ee9c122ba6c1743c8d0d81a823
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-06-14 15:03:22 -0300

    form-recuperar_senha sendo feito

---
+++ form-recuperar-senha.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de recuperação de senha</title>
+</head>
+<body>
+
+<form action="recuperar.php" method="post">
+
+<label >Email: <input type="email" name="email"></label>
+<input type="submit" value="Enviar">
+
+
+</form>
+    
+</body>
+</html>

53d9c535406af138a0765b9172757ac897c7703d
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-06-14 15:06:45 -0300

    update formrecsen

--- form-recuperar-senha.php
+++ form-recuperar-senha.php
@@ -6,10 +6,11 @@
     <title>Formulário de recuperação de senha</title>
 </head>
 <body>
+    Digite o seu email para gerar uma nova senha: <br>
 
 <form action="recuperar.php" method="post">
 
-<label >Email: <input type="email" name="email"></label>
+<label >Email: <input type="email" name="email"></label><br>
 <input type="submit" value="Enviar">
 
 
